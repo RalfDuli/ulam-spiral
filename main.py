b8c94a7f2288057901e7386fe44ac8133bff52c8
RalfDuli <ralfduli24@gmail.com>
2023-09-09 17:21:11 +0100

    Implemented GUI in Tkinter

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-import pygame
+import numpy as np
+import tkinter as tk
 
 from ulam_spiral import *
+from constants import *
 
-spiral = UlamSpiral(10)
-pygame.init()
+spiral_dim = 12
+spiral = UlamSpiral(spiral_dim)
+
+scaling_factor = 50
+
+def draw_spiral():
+    canvas = tk.Canvas(root, width=SCREEN_X, height=MENU_HEIGHT+SCREEN_Y)
+    canvas.pack()
+    for row in (range(spiral.dim)):
+        for col in (range(spiral.dim)):
+            if spiral.matrix[row][col] == 1: color = 'black'
+            else: color = 'white'
+            x1 = col * scaling_factor
+            x2 = (col+1) * scaling_factor
+            y1 = MENU_HEIGHT + row * scaling_factor
+            y2 = MENU_HEIGHT + (row+1) * scaling_factor
+            canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+            #pygame.draw.rect(screen, color, (col * scaling_factor,
+            #                                 row * scaling_factor,
+             #                                scaling_factor, scaling_factor))
+
+root = tk.Tk()
+root.title("Ulam Spiral")
+root.geometry(f"{SCREEN_X}x{SCREEN_Y}")
+
+draw_spiral()
+
+
+root.mainloop()
+
+""" pygame.init()
 
 scaling_factor = 50
 
@@ -19,12 +50,6 @@ while running:
 
     screen.fill((255, 255, 255))
 
-    for row in (range(spiral.dim)):
-        for col in (range(spiral.dim)):
-            if spiral.matrix[row][col] == 1: color = (0,0,0)
-            else: color = (255,255,255)
-            pygame.draw.rect(screen, color, (col * scaling_factor,
-                                             row * scaling_factor,
-                                             scaling_factor, scaling_factor))
+    draw_spiral()
 
-    pygame.display.flip()
+    pygame.display.flip() """
