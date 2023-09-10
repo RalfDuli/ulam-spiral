70e174f6ad4353416869fa53a11c0d01d1d9aa16
RalfDuli <ralfduli24@gmail.com>
2023-09-10 17:20:55 +0100

    Allowed user to change spiral dimensions

--- main.py
+++ main.py
@@ -1,55 +1,65 @@
-import numpy as np
 import tkinter as tk
-
 from ulam_spiral import *
 from constants import *
 
-spiral_dim = 12
-spiral = UlamSpiral(spiral_dim)
+starting_dim = 5
+spiral = UlamSpiral(starting_dim)
 
-scaling_factor = 50
-
-def draw_spiral():
-    canvas = tk.Canvas(root, width=SCREEN_X, height=MENU_HEIGHT+SCREEN_Y)
-    canvas.pack()
-    for row in (range(spiral.dim)):
-        for col in (range(spiral.dim)):
-            if spiral.matrix[row][col] == 1: color = 'black'
-            else: color = 'white'
+def draw_spiral(canvas):
+    scaling_factor = SCREEN_X / (spiral.dim * 1.25)
+    canvas.delete("all")  # Clear the canvas
+    for row in range(spiral.dim):
+        for col in range(spiral.dim):
+            if spiral.matrix[row][col] == 1:
+                color = 'black'
+            else:
+                color = 'white'
             x1 = col * scaling_factor
-            x2 = (col+1) * scaling_factor
+            x2 = (col + 1) * scaling_factor
             y1 = MENU_HEIGHT + row * scaling_factor
-            y2 = MENU_HEIGHT + (row+1) * scaling_factor
+            y2 = MENU_HEIGHT + (row + 1) * scaling_factor
             canvas.create_rectangle(x1, y1, x2, y2, fill=color)
-            #pygame.draw.rect(screen, color, (col * scaling_factor,
-            #                                 row * scaling_factor,
-             #                                scaling_factor, scaling_factor))
+
+def change_dimension(new_dim):
+    spiral.change_dim(new_dim)
+    draw_spiral(canvas)
 
 root = tk.Tk()
 root.title("Ulam Spiral")
 root.geometry(f"{SCREEN_X}x{SCREEN_Y}")
 
-draw_spiral()
+prompt = tk.Label(text="Select the dimensions of the Ulam Spiral")
+prompt.pack(side = 'top')
 
+button_bar = tk.Frame(root)
+button_bar.pack(side='top')
+
+five_dim_button = tk.Button(button_bar, text='5x5', width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                            command=lambda: change_dimension(5))
+five_dim_button.pack(side='left')
+
+ten_dim_button = tk.Button(button_bar, text='10x10', width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                           command=lambda: change_dimension(10))
+ten_dim_button.pack(side='left')
+
+twelve_dim_button = tk.Button(button_bar, text='12x12', width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                              command=lambda: change_dimension(12))
+twelve_dim_button.pack(side='left')
+
+fifteen_dim_button = tk.Button(button_bar, text='15x15', width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                               command=lambda: change_dimension(15))
+fifteen_dim_button.pack(side='left')
+
+twenty_dim_button = tk.Button(button_bar, text='20x20', width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                             command=lambda: change_dimension(20))
+twenty_dim_button.pack(side='left')
+
+fifty_dim_button = tk.Button(button_bar, text='50x50', width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
+                             command=lambda: change_dimension(50))
+fifty_dim_button.pack(side='left')
+
+canvas = tk.Canvas(root, width=SCREEN_X, height=SCREEN_Y - MENU_HEIGHT)
+canvas.pack()
+draw_spiral(canvas)
 
 root.mainloop()
-
-""" pygame.init()
-
-scaling_factor = 50
-
-screen = pygame.display.set_mode([spiral.dim * scaling_factor,
-                                spiral.dim * scaling_factor])
-
-running = True
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill((255, 255, 255))
-
-    draw_spiral()
-
-    pygame.display.flip() """
